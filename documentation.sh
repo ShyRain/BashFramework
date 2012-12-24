90a4286b464b5d480a7024e1dae045cabdd59bdb
Emre Çamalan <emre@camalan.net>
2012-12-24 10:57:06 +0200

    dökümanların içeriğinde yer alan dökükmantasyonu toplayacak script olarak documentation.sh eklendi

--- documentation.sh
+++ documentation.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+source ${__pwd}inc/header.inc
+
+## Framework dosyalarındaki açıklamaları topla
+_documentation="documentation.txt"
+_liste=`ls -1 | grep '.sh'`
+for _script in ${_liste}
+do 
+	echo '---------------------------------------' >> ${__pwd}${_documentation}
+	echo "Script Name : "$0 >> ${__pwd}${_documentation}
+	cat ${_script} | grep '####' >> ${__pwd}${_documentation}
+	echo '---------------------------------------' >> ${__pwd}${_documentation}
+	echo '' >> ${__pwd}${_documentation}
+	echo '' >> ${__pwd}${_documentation}
+done
